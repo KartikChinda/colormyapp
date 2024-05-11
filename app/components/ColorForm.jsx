466a61fa8c5accd007823131ebcd56db061fbb4a
Kartik Chinda <kartik.chinda@gmail.com>
2024-05-11 09:57:36 +0530

    Created form layout (responsive)

--- app/components/ColorForm.jsx
+++ app/components/ColorForm.jsx
@@ -4,8 +4,72 @@ const ColorForm = () => {
     return (
 
         <>
-            <div>Form to submit here</div>
-            <div className='bg-[#ffc700] border-2 border-black rounded-xl'>This is another div</div>
+            <section className='flex flex-col justify-start items-start pt-8 border-2 border-black rounded-md'>
+                <div className='hover:skew-y-1 hover:translate-x-2 font-heading pb-10 mx-auto'>
+                    <span className='text-md xl:text-xl font-semibold border-2 rounded-2xl p-2 border-black hover:font-bold hover:shadow-xl shadow-lg '>Create your own palette</span>
+                </div>
+                <div className='w-[90%] ml-1'>
+                    <form action="" className='flex flex-col items-start justify-start gap-2 mb-10 ' >
+                        <div className='flex xl:text-lg font-heading flex-col justify-start items-start gap-1 border-2 px-2 ml-2 text-sm rounded-xl py-1 w-full hover:-translate-x-2 hover:-skew-y-1 hover:shadow-md'>
+                            <label>
+                                Primary color
+                            </label>
+                            <div>
+                                <input placeholder='Enter color here' className=''>
+
+                                </input>
+                            </div>
+                        </div>
+
+                        {/* color 2 */}
+                        <div className='flex xl:text-lg font-heading flex-col justify-start items-start gap-1 border-2 px-2 ml-2 text-sm rounded-xl py-1 w-full hover:-translate-x-2 hover:-skew-y-1 hover:shadow-md'>
+                            <label>
+                                Secondary color
+                            </label>
+                            <div>
+                                <input placeholder='Enter color here' className=''>
+
+                                </input>
+                            </div>
+                        </div>
+
+                        {/* Color 3 */}
+                        <div className='flex xl:text-lg font-heading flex-col justify-start items-start gap-1 border-2 px-2 ml-2 text-sm rounded-xl py-1 w-full hover:-translate-x-2 hover:-skew-y-1 hover:shadow-md'>
+                            <label>
+                                Tertiary color
+                            </label>
+                            <div>
+                                <input placeholder='Enter color here' className=''>
+
+                                </input>
+                            </div>
+                        </div>
+
+                        {/* color 4 */}
+                        <div className='flex xl:text-lg font-heading flex-col justify-start items-start gap-1 border-2 px-2 ml-2 text-sm rounded-xl py-1 w-full hover:-translate-x-2 hover:-skew-y-1 hover:shadow-md'>
+                            <label>
+                                Quarternery color
+                            </label>
+                            <div>
+                                <input placeholder='Enter color here' className=''>
+
+                                </input>
+                            </div>
+                        </div>
+                        <div className='flex flex-col justify-center items-center mx-auto gap-2 mt-5'>
+                            <button className='text-md xl:text-xl font-semibold border-2 rounded-2xl p-2 border-black hover:font-bold hover:shadow-xl shadow-lg font-heading px-4 hover:text-[21px] bg-[#ffc700]'>Showcase </button>
+                            <button className='text-md xl:text-xl font-semibold border-2 rounded-2xl p-2 border-black hover:font-bold hover:shadow-md font-heading px-4 hover:text-[18px]'> Clear </button>
+                        </div>
+
+
+                    </form>
+                </div>
+
+
+
+
+
+            </section>
         </>
     )
 }
--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ import ColorForm from "./components/ColorForm";
 export default function Home() {
   return (
     <>
-      <div className="border-4 border-black w-[90%] md:mt-14 px-3 mt-5 mx-auto mb-12 flex flex-col md:flex-row bg-[#e6e5db] md:justify-around py-5">
-        <div className="border-4 border-black md:w-[62%] p-5 bg-[#fff] ">
+      <div className="border-4 border-black w-[90%] md:mt-14 px-3 mt-5 mx-auto mb-12 flex flex-col lg:flex-row gap-5 lg:gap-0 bg-[#e6e5db] lg:justify-around py-5">
+        <div className="border-4 border-black lg:w-[62%] p-5 bg-[#fff] ">
           <HomePage />
         </div>
-        <div className="border-4 border-black md:w-[32%] p-5 text-center bg-[#fff] ">
+        <div className="border-4 border-black lg:w-[32%] p-5 text-center bg-[#fff] ">
           <ColorForm />
         </div>
       </div>
